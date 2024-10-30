54b0cfc37007ecd06a93a80d1f1c698970f296a0
jmuncy2 <71195274+jmuncy2@users.noreply.github.com>
2024-10-30 20:41:40 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.sidebar.title("Menu")
+menu = st.sidebar.radio("Select a page:", ["Home", "Results", "About"])
+
+if menu == "Home":
+    st.title("Welcome to the Decypher App Showcase")
+    st.write(
+        "This website is to showcase the results our group has made on our Final Project."
 )
+
+if menu == "Results":
+    st.title("Results")
+    st.write(
+        "This page is dedicated to showcasing our results and charts."
+)
+
+elif menu == "About":
+    st.title("About This App")
+    st.write(
+        "Information about app."
+    )
+
