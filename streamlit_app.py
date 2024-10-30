3bc2b0c0030f13c6ed2e6e76c448cd4d8c2ea3f8
jmuncy2 <71195274+jmuncy2@users.noreply.github.com>
2024-10-30 21:08:15 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+
 
 st.sidebar.title("Menu")
 menu = st.sidebar.radio("Select a page:", ["Home", "Results", "About"])
@@ -14,6 +17,29 @@ if menu == "Results":
     st.write(
         "This page is dedicated to showcasing our results and charts."
 )
+
+    data = pd.read_csv('modelperformance.csv')
+    models = data['Model'].unique()
+
+    
+    plt.figure(figsize=(15, 10))
+
+    for i, model in enumerate(models):
+        model_data = data[data['Model'] == model]
+        epochs = model_data['Epoch']
+
+        plt.subplot(len(models), 1, i + 1)
+        plt.plot(epochs, model_data['Train Accuracy'], label='Train Accuracy')
+        plt.plot(epochs, model_data['Validation Accuracy'], label='Validation Accuracy')
+        plt.title(f'{model} - Accuracy')
+        plt.xlabel('Epochs')
+        plt.ylabel('Accuracy')
+        plt.ylim(35, 100)
+        plt.legend()
+
+    plt.tight_layout()
+    st.pyplot(plt.gcf())
+
 
 elif menu == "About":
     st.title("About This App")
